87053c95e68c3a511aaee5d130c1e9eff9b463df
soler <gorgue@correo.ugr.es>
2018-03-13 17:56:31 +0100

    first commit

---
+++ practicasThreeSGDelete/grua/Robot.js
@@ -0,0 +1,38 @@
+class Robot extends THREE.Object3D {
+
+    constructor(){
+        super();
+        this.arm = null;
+        this.head = null;
+        this.body = null;
+        this.robot = null;
+        this.height
+        this.material    = (parameters.material === undefined ? new THREE.MeshPhongMaterial ({color: 0xd4af37, specular: 0xfbf804, shininess: 70}) : parameters.material);
+
+
+
+    }
+
+
+
+
+    createArm(){
+
+        var geometry = new THREE.BoxGeometry (1,1,1);
+        var material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
+        this.arm.add(new THREE.Mesh (
+                geometry,material));
+        geometry = new THREE.CylinderGeometry()
+
+        asas
+    }
+
+    createHead(){
+
+    }
+    createBody(){
+
+    }
+    createRobot(){}
+
+}
