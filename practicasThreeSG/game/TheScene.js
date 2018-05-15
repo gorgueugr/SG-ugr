4af8c11d62809d236cf0b70fa213fe56c4f796f7
jorge <gorgue@correo.ugr.es>
2018-05-15 13:16:09 +0200

    Testing stick

--- practicasThreeSG/game/TheScene.js
+++ practicasThreeSG/game/TheScene.js
@@ -18,6 +18,7 @@ class TheScene extends Physijs.Scene {
       this.terrainScene = null;
       this.tree = null;
       this.player = null;
+      this.ball = null;
 
       this.clock = new THREE.Clock();
       this.mixers = [];
@@ -446,6 +447,7 @@ class TheScene extends Physijs.Scene {
 
     createModels() {
         this.player = new Player(this);
+        this.ball = new Ball(this);
 
         //this.actualView = this.player.view;
 
